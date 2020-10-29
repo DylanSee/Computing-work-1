6c6baaa2cd2bc1a51f15a3eeb376749ea5b899f8
DylanSee <69779263+DylanSee@users.noreply.github.com>
2020-10-30 03:20:58 +0800

    Update Math Revision.py

--- Math Revision.py
+++ Math Revision.py
@@ -1,6 +1,8 @@
 print("Title of program: Simple math Revision")
 print()
 print("Welcome to this wonderful math revision website! Please answer the following questions truthfully and you to test your math calculation skills!")
+print("This math revision is for you to exercise quick mental calculation.")
+print("Therefore, please refrain from using a calculator to increase the difficulty of the math revision. Thank you!")
 print("Please respond with an answer")
 print()
 points = 0
@@ -9,47 +11,46 @@ if Question1 == 125:
   print("Great!")
   points += 1
 else:
-  print("Oops! Your answer is wrong")
+  print("Oops! Your answer is wrong. Continue to work hard!")
 
 Question2 = int(input("what is the answer for 23x5-(70-1)?"))
 if Question2 == 46:
   print("Great Job!")
   points += 1
 else:
-  print("Oops! Your answer is wrong")
+  print("Oops! Your answer is wrong. Don't give up yet!")
 
 Question3 = int(input("what is the answer for 34-67+689+(69x3)?"))
 if Question3 == 863:
   print("Yay! You got it correct!")
   points += 1
 else:
-  print("Oops! Your answer is wrong") 
+  print("Oops! Your answer is wrong. Keep trying!") 
 
 Question4 = int(input("What is the answer for 68x2-(88/4+2)+995? (/means divide)"))  
 if Question4 == 133:  
   print("Yay! You got it correct!")
   points += 1
 else:
-  print("Oops! Your answer is wrong") 
+  print("Oops! Your answer is wrong. You can do better than this!") 
 
 Question5 = int(input("What is the answer for 69+111+(629x4)?"))
 if Question5 == 2696: 
   print("Hooray! You did it!")
   points += 1
 else:
-  print("Oops! Your answer is wrong")
+  print("Oops! Your answer is wrong. Work harder!")
 
-Question6 = int(input("What is the answer for 45+332-38+(4545/9)-22x3+95x4? (/ means divide)
+Question6 = int(input("What is the answer for 45+332-38+(4545/9)-22x3+95x4? (/ means divide)"))
 if Question6 == 1158:
   print("Yay, you have done it!")
   points += 1
 else:
-  print("Oops! Your answer is wrong")
+  print("Oops! Your answer is wrong. You should learn from your mistakes and improve.")
   
 print("You got " + str(points) + " / 5 !")
 if str(points) == 5
   print("WooHoo! Great Job! Keep it up!")
 else:("Try better next time")
-
 
   
